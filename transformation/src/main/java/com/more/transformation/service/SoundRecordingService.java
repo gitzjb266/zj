87d73b9cfbe376a46fb558ab0c45051bf8fefb58
gitzjb266 <52900599+gitzjb266@users.noreply.github.com>
2021-08-13 16:01:19 +0800

    Delete transformation directory

--- transformation/src/main/java/com/more/transformation/service/SoundRecordingService.java
+++
@@ -1,7 +0,0 @@
-package com.more.transformation.service;
-
-import java.io.FileNotFoundException;
-
-public interface SoundRecordingService {
-    String saveFile(String storageType,String originalFilename,String filePath) throws FileNotFoundException;
-}
